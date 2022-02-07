4eceac2468a325b3931501502ce0b0d22530bfa8
Anel <44476653+anme98@users.noreply.github.com>
2022-02-07 16:23:05 +0100

    add entity ToDo

--- src/main/java/de/anybytes/springbootschulung/entity/ToDo.java
+++ src/main/java/de/anybytes/springbootschulung/entity/ToDo.java
@@ -1,13 +1,20 @@
 package de.anybytes.springbootschulung.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class ToDo {
 
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+
+    private Boolean isDone;
+    private String name;
+    private String dueDate;
 
     public ToDo() {
     }
@@ -18,5 +25,29 @@ public class ToDo {
 
     public Long getId() {
         return id;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
     }
 }
