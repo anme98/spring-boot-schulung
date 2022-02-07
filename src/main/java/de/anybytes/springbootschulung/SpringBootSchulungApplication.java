60aa79181edf7d9a85dd171b086a345c87a39d4d
Anel <44476653+anme98@users.noreply.github.com>
2022-02-07 15:49:48 +0100

    init

---
+++ src/main/java/de/anybytes/springbootschulung/SpringBootSchulungApplication.java
@@ -0,0 +1,13 @@
+package de.anybytes.springbootschulung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchulungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSchulungApplication.class, args);
+    }
+
+}
